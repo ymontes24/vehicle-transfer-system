f41f00c30835155c9e5643b0e329c0255e07b783
yeison-montes_mmcgh <yeison.montes@mmc.com>
2025-05-23 17:52:33 -0500

    add necessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { OrganizationalUnitsModule } from './organizational-units/organizational-units.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { ProjectsModule } from './projects/projects.module';
+import { RolesModule } from './roles/roles.module';
+import { TransfersModule } from './transfers/transfers.module';
+import { UsersModule } from './users/users.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(databaseConfig)
+    TypeOrmModule.forRoot(databaseConfig),
+    AuthModule,
+    OrganizationalUnitsModule,
+    PermissionsModule,
+    ProjectsModule,
+    RolesModule,
+    TransfersModule,
+    UsersModule,
+    VehiclesModule
   ],
   controllers: [AppController],
   providers: [AppService],
